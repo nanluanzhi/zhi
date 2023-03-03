bc9e6b9701f87152f43226e1807ae69301e1d3b4
nanluanzhi <2533395371@qq.com>
2023-03-03 22:11:54 +0800

    1

---
+++ .vscode/C语言/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+int main()
+{
+    printf("hello world");
+    return 0;
+}
